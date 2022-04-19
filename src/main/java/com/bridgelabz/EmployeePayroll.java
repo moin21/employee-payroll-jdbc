17fa54f94fd700eb80097e1ff789672923cacbe5
moin21 <mamoinuddin@gmail.com>
2022-04-19 16:29:56 +0530

    Added: Logic to updtate employee salary

--- src/main/java/com/bridgelabz/EmployeePayroll.java
+++ src/main/java/com/bridgelabz/EmployeePayroll.java
@@ -54,6 +54,110 @@ public class EmployeePayroll {
         this.netPay = netPay;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getBasicPay() {
+        return basicPay;
+    }
+
+    public void setBasicPay(double basicPay) {
+        this.basicPay = basicPay;
+    }
+
+    public double getDeductions() {
+        return deductions;
+    }
+
+    public void setDeductions(double deductions) {
+        this.deductions = deductions;
+    }
+
+    public double getTaxablePay() {
+        return taxablePay;
+    }
+
+    public void setTaxablePay(double taxablePay) {
+        this.taxablePay = taxablePay;
+    }
+
+    public double getIncomeTax() {
+        return incomeTax;
+    }
+
+    public void setIncomeTax(double incomeTax) {
+        this.incomeTax = incomeTax;
+    }
+
+    public double getNetPay() {
+        return netPay;
+    }
+
+    public void setNetPay(double netPay) {
+        this.netPay = netPay;
+    }
+
     /**
      * To String method to return ArrayList to Strings.
      */
--- src/main/java/com/bridgelabz/EmployeePayrollJDBCMain.java
+++ src/main/java/com/bridgelabz/EmployeePayrollJDBCMain.java
@@ -12,11 +12,12 @@ public class EmployeePayrollJDBCMain {
      * Created Configuration object - configuration.
      * Created Statements object using configuration.establishConnection method(returns connection) - statements
      * Calling retrievePayroll method for statements object. To print Table rows to console.
+     * Calling updatePayroll method to update table row.
      */
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
         Statements statements = new Statements(configuration.establishConnection());
         statements.retrievePayroll();
-
+        statements.updatePayroll();
     }
 }
